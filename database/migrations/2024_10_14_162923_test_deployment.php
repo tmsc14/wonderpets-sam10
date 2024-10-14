0247b1236e97a820e613cf0f136addd58d15906a
tmsc14 <cabanayan.tereemari@auf.edu.ph>
2024-10-15 00:33:40 +0800

    test-deployment

---
+++ database/migrations/2024_10_14_162923_test_deployment.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('test', function (Blueprint $table) {
+            $table->id();
+            $table->string('deploy')->index();
+            $table->longText('status');
+            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('reserved_at')->nullable();
+            $table->unsignedInteger('available_at');
+            $table->unsignedInteger('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('test');
+    }
+};
