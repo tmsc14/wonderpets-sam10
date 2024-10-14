06929d5502d161156404b54dbc153bd1e21ce0d0
tmsc14 <cabanayan.tereemari@auf.edu.ph>
2024-10-15 00:47:53 +0800

    test-deployment-v2

--- database/migrations/2024_10_14_162923_test_deployment.php
+++ database/migrations/2024_10_14_162923_test_deployment.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('test', function (Blueprint $table) {
+        Schema::create('test', function (Blueprint $table) {
             $table->id();
             $table->string('deploy')->index();
             $table->longText('status');
