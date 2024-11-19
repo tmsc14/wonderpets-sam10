da5ba71696ba24420488fcbee80670475d885a2b
tmsc14 <teree.cabanayan14@gmail.com>
2024-11-19 11:32:26 +0800

    added dompdf

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,15 +3,21 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Barryvdh\DomPDF\Facade as PDF; // Import PDF facade
+use Barryvdh\DomPDF\ServiceProvider as DomPDFServiceProvider; // Import DomPDF Service Provider
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function register()
     {
-        //
+        // Register DomPDF service provider
+        $this->app->register(DomPDFServiceProvider::class);
+        
+        // Register PDF alias
+        $this->app->alias('PDF', PDF::class);
     }
 
     /**
